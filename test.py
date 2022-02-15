475d458ab445c180b9ee26ae86e3261acbef83be
mini <itman390@gmail.com>
2022-02-15 20:56:57 +0900

    fixed .gitignore

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import pprint
+import time
 
 import pyupbit
 
@@ -8,14 +9,24 @@ ACCESS_KEY = lines[0].strip()
 SECRET_KEY = lines[1].strip()
 f.close()
 
-# # upbit class instance
-# upbit = pyupbit.Upbit(ACCESS_KEY, SECRET_KEY)
+TICKER = 'KRW-ONG'
+
+# upbit class instance
+upbit = pyupbit.Upbit(ACCESS_KEY, SECRET_KEY)
 #
 # resp = upbit.get_individual_order('8c67b3a4-33ac-475a-a181-42bf1c77db30')
-#
-# # pprint.pprint(resp)
+order_list = upbit.get_order(ticker_or_uuid=TICKER)
+for order in order_list:
+    uuid = order['uuid']
+    print(uuid)
+    upbit.cancel_order(uuid=uuid)
+pprint.pprint(order_list)
+time.sleep(1)
+amount = upbit.get_balance_t(ticker=TICKER)
+print(amount)
+upbit.sell_market_order(ticker=TICKER, volume=amount)
+order_list = upbit.get_order(ticker_or_uuid=TICKER)
+pprint.pprint(order_list)
 # pprint.pprint(resp)
 # print(resp['uuid'])
 
-orderbook = pyupbit.get_orderbook('KRW-ONG')
-pprint.pprint(orderbook)
