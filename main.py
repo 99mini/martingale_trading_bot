f6036892fe1bb3281c1e0d80783065d74c199ade
mini <itman390@gmail.com>
2022-02-15 20:39:27 +0900

    fix consolo log & add telegram

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import datetime
 import time
 
 import pyupbit
+import requests
+
+from config import *
 
 all_ticker_url = "https://api.upbit.com/v1/ticker"
 
@@ -14,6 +17,15 @@ f.close()
 
 # upbit class instance
 upbit = pyupbit.Upbit(ACCESS_KEY, SECRET_KEY)
+
+# 텔레그램 메세지 보내기
+def telegramMassageBot(msg):
+    params = {'chat_id': telebotid, 'text': msg}
+    # 텔레그램으로 메시지 전송
+    try:
+        requests.get(teleurl, params=params)
+    except Exception as e:
+        print('telegram error: ', e)
 
 
 # 매수 주문
@@ -59,17 +71,16 @@ def sell_order(ticker, price, volume):
 '''
 
 # init setting
-TICKER = 'KRW-ONG'
+TICKER = 'KRW-BORA'
 ONE_ORDER_AMOUNT = 5_050
 TOP = 1250
-BOTTOM = 1000
-INTERVAL = 10
+BOTTOM = 1150
+INTERVAL = 15
 TICK = 5
 
-cur_price = pyupbit.get_current_price(ticker=TICKER)
-base_price = cur_price - INTERVAL
+cur_price = pyupbit.get_current_price(ticker=TICKER) + TICK
+base_price = cur_price
 order_volume = ONE_ORDER_AMOUNT / cur_price
-sell_order_uuid_list = set()
 
 buy_uuid = buy_order(ticker=TICKER,
                      price=cur_price,
@@ -81,15 +92,15 @@ if not upbit.get_individual_order(buy_uuid)['trades']:
     upbit.cancel_order(buy_uuid)
     exit()
 
-sell_order_uuid_list.add(sell_order(ticker=TICKER,
-                                    price=cur_price + INTERVAL,
-                                    volume=order_volume))
-print(*sell_order_uuid_list)
+sell_order(ticker=TICKER,
+           price=cur_price + INTERVAL,
+           volume=order_volume)
+
 print("#" * 100)
 print("매수: {0}".format(cur_price))
 print("예약 매도: {0}".format(cur_price + INTERVAL))
 print("수량: {0}".format(order_volume))
-print("목표가: {0}".format(base_price))
+print("목표가: {0} | {1}".format(base_price - INTERVAL, base_price + INTERVAL))
 print("#" * 100)
 
 while True:
@@ -101,11 +112,18 @@ while True:
             break
 
         # 기준가 도달시 매수와 예약 매도 실행
-        if base_price == cur_price:
+        # 가격 하락시 매수
+        if base_price - INTERVAL == cur_price + TICK or base_price + INTERVAL == cur_price + TICK:
             time.sleep(1)
             # 구매 주문
+            # 하락시
+            if base_price - INTERVAL == cur_price + TICK:
+                order_price = base_price - INTERVAL
+            # 상승시
+            else:
+                order_price = base_price + INTERVAL
             buy_uuid = buy_order(ticker=TICKER,
-                                 price=cur_price,
+                                 price=order_price,
                                  volume=order_volume)
 
             # 구매가 안되었을 경우 기다린다
@@ -117,76 +135,44 @@ while True:
                 continue
 
             # 예약 매도 주문
-            sell_order_uuid_list.add(sell_order(ticker=TICKER,
-                                                price=cur_price + INTERVAL,
-                                                volume=order_volume))
+            sell_order(ticker=TICKER,
+                       price=order_price + INTERVAL,
+                       volume=order_volume)
 
-            base_price -= INTERVAL
+            base_price = order_price
             print("매수: {0}".format(cur_price))
             print("예약 매도: {0}".format(cur_price + INTERVAL))
             print("수량: {0}".format(order_volume))
-            print("목표가: {0}".format(base_price))
+            print("목표가: {0} | {1}".format(base_price - INTERVAL, base_price + INTERVAL))
             print("#" * 100)
-
-        # 미채결 물량이 있는 경우
-        # TODO main error... Set changed size during iteration
-        for uuid in sell_order_uuid_list:
-            resp = upbit.get_individual_order(uuid)
-            # 체결이 된 경우
-            # 같은 가격에 매수를 진행하고
-            # 기준가를 다시 설정한다.
-            if resp is not None and len(resp['trades']) > 0:
-                sell_price = float(resp['trades'][0]['price'])
-                sell_order_uuid_list.remove(resp['uuid'])
-
-                time.sleep(1)
-                # 구매 주문
-                buy_uuid = buy_order(ticker=TICKER,
-                                     price=sell_price,
-                                     volume=order_volume)
-
-                # 구매가 안되었을 경우 기다린다
-                if not upbit.get_individual_order(buy_uuid)['trades']:
-                    print('not init buy......')
-                    time.sleep(10)
-                    # 매수 취소 후 continue
-                    upbit.cancel_order(buy_uuid)
-                    continue
-
-                # 예약 매도 주문
-                sell_order_uuid_list.add(sell_order(ticker=TICKER,
-                                                    price=sell_price + INTERVAL,
-                                                    volume=order_volume))
-                base_price = sell_price - INTERVAL
-
-                print("\n매도 체결: {0}".format(sell_price))
-                print("기준 가격 갱신: {0}".format(base_price))
-                print("#" * 100)
-                print("\n매수: {0}".format(cur_price))
-                print("예약 매도: {0}".format(cur_price + INTERVAL))
-                print("수량: {0}".format(order_volume))
-                print("#" * 100)
 
         if datetime.datetime.now().second % 10 == 0:
             print("=" * 100)
+            print(datetime.datetime.now())
             print("기준 가격: {0}".format(base_price))
             print("현재 가격: {0}".format(cur_price))
+            print("목표가: {0} | {1}".format(base_price - INTERVAL, base_price + INTERVAL))
             print("목표 인터벌: {0}".format(INTERVAL))
 
-            print("=" * 45 + '미채결물량' + "=" * 45)
-            for uuid in sell_order_uuid_list:
-                resp = upbit.get_individual_order(uuid)
-                sell_price = float(resp['price'])
-                print(sell_price)
-            print("=" * 100)
         time.sleep(1)
     except Exception as e:
         print("main error...", e)
 
-# 프로그램 종료시 전량 매도
-for uuid in sell_order_uuid_list:
-    upbit.cancel_order(uuid)
-amount = upbit.get_amount(ticker=TICKER)
+# 미채결 주문 취소
+order_list = upbit.get_order(ticker_or_uuid=TICKER)
+for order in order_list:
+    uuid = order['uuid']
+    print(uuid)
+    upbit.cancel_order(uuid=uuid)
+time.sleep(1)
+
+# 전량 매도
+amount = upbit.get_balance_t(ticker=TICKER)
 upbit.sell_market_order(ticker=TICKER, volume=amount)
+order_list = upbit.get_order(ticker_or_uuid=TICKER)
+
+# 텔레그렘 메세지
+msg='체널이탈로 인한 종료'
+telegramMassageBot(msg=msg)
 print("전량 매도")
 print("프로그램 종료")
